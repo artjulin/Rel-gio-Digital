0c480783ac9a01111795a0432a52fede9f2cbf4a
artjulin <hortiz.julio@escola.pr.gov.br>
2025-09-03 08:35:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let horas = agora.getHours().toString().padStart (2, '0');
 let minutos = agora.getMinutes().toString().padStart (2, '0');
 let segundos = agora.getSeconds().toString().padStart (2, '0');
   
-const horaAtual = '$(horas):$(minutos):$(segundos);
+const horaAtual = `$(horas):$(minutos):$(segundos)`;
   
 document.getElementById('clock').textContent = horaAtual';
 }
